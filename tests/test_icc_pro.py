e109a4d324adb3489cbb392c78d5df1e6efc7186
Zeheng Li <imzehengl@gmail.com>
2024-06-13 10:35:27 -0600

    fix tests

--- tests/test_icc_pro.py
+++ tests/test_icc_pro.py
@@ -4,7 +4,7 @@ from icc_pro import ICC_PRO
 
 
 def test_icc_pro_init(iccpro):
-    assert len(ICC_PRO.list_generic_endpoints()) == 8
+    assert len(ICC_PRO.list_generic_endpoints()) == 9
 
     host = getenv("iccpro_host")
     username = getenv("iccpro_username")
@@ -18,5 +18,5 @@ def test_icc_pro_init(iccpro):
     assert iccpro.client_id == client_id
     assert iccpro.client_secret == client_secret
 
-    assert len(iccpro.list_generic_endpoints()) == 8
-    assert len(iccpro.list_detailed_endpoints()) == 8
+    assert len(iccpro.list_generic_endpoints()) == 9
+    assert len(iccpro.list_detailed_endpoints()) == 9
